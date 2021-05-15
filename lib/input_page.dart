1394eef80154f621d116e4089662380ad98edf24
Rich <rich.chang@EgisTec.com>
2021-05-15 20:58:46 +0800

    Refactor widget by extracting widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,50 +16,20 @@ class _InputPageState extends State<InputPage> {
         children: [
           Expanded(child: Row(
               children: [
-                Expanded(child: Container (
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                Expanded(child: ReusableContainer(),
                 ),
-                Expanded(child: Container (
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                Expanded(child: ReusableContainer(),
                 ),
               ],
             ),
           ),
-          Expanded(child: Container (
-              margin: EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
+          Expanded(child: ReusableContainer(),
           ),
           Expanded(child: Row(
               children: [
-                Expanded(child: Container (
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                Expanded(child: ReusableContainer(),
                 ),
-                Expanded(child: Container (
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                Expanded(child: ReusableContainer(),
                 ),
               ],
             ),
@@ -67,5 +37,22 @@ class _InputPageState extends State<InputPage> {
         ],
       )
     );
+  }
+}
+
+class ReusableContainer extends StatelessWidget {
+  const ReusableContainer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container (
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      );
   }
 }
