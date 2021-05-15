7e19c0e8ac6c9c1f75a38bf4ca531c177ee68166
Rich <rich.chang@EgisTec.com>
2021-05-15 20:42:03 +0800

    Add border radius by BoxDecoration widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -15,6 +15,9 @@ class _InputPageState extends State<InputPage> {
       body: Container (
         color: Color(0xFF1D1E33),
         margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
         height: 200.0,
         width: 170.0,
       )
