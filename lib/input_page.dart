59791c7d7610c571a4781fb635c39ba82daf8b6a
Rich <rich.chang@EgisTec.com>
2021-05-25 15:33:39 +0800

    Set the baseline property of text to align

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -76,6 +76,8 @@ class _InputPageState extends State<InputPage> {
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
                       children: [
                         Text(
                           "180", style: kNumberTextStyle,
