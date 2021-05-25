531cfc8aced58d8b632a1064de54cd8a574b4dbc
Rich <rich.chang@EgisTec.com>
2021-05-25 21:17:40 +0800

    User ThemeData class to customize Materail component

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -91,18 +91,28 @@ class _InputPageState extends State<InputPage> {
                         )
                       ],
                     ),
-                    Slider(
-                      value: height.toDouble(),
-                      min: 120.0,
-                      max: 220.0,
-                      activeColor: Color(0xFFEB1555),
-                      inactiveColor: Color(0xFF8D8E98),
-                      onChanged: (double pos){
-                        print(pos);
-                        setState(() { //To rebuild the parent widget UI when pos changes
-                          height = pos.round();
-                        });
-                      },
+
+                    // Actual property and function in this component
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        activeTrackColor: Colors.white,
+                        inactiveTrackColor: Color(0xFF8D8E98),
+                        thumbColor: Color(0xFFEB1555),
+                        overlayColor: Color(0x29EB1555),
+                        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                      ),
+                      child: Slider(
+                        value: height.toDouble(),
+                        min: 120.0,
+                        max: 220.0,
+                        onChanged: (double pos){
+                          print(pos);
+                          setState(() { //To rebuild the parent widget UI when pos changes
+                            height = pos.round();
+                          });
+                        },
+                      ),
                     ),
                   ],
                 ),
