3759a4a8f13e7eeefae39353eb877951d39b9ffb
Rich <rich.chang@EgisTec.com>
2021-05-29 16:20:22 +0800

    實作BMI result頁面的基礎UI元件

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/constant.dart';
+import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -5,17 +7,53 @@ class ResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.red,
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: RaisedButton(
-          color: Colors.red,
-          child: Text('BACK to HOME'),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Your Result',
+            style: kTitleStyle,
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableContainer(
+                bkgColor: kActiveCardColor,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Normal',
+                      style: kResultTextStyle,
+                    ),
+                    Text(
+                      '21.1',
+                      style: kBmiNumberStyle,
+                    ),
+                    Text(
+                      'You have to eat more.',
+                      //textAlign: TextAlign.center,
+                      style: kBodyTextStyle,
+                    ),
+                  ],
+                )),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              child: Center(child: Text("Re-CALCULATE", style: kLargeButtonStyle)),
+              color: kBottomContainerColor,
+              height: kBottomContainerHeight,
+              width: double.infinity,
+              padding: EdgeInsets.only(bottom: 20.0),
+            ),
+          )
+        ],
       ),
     );
   }
